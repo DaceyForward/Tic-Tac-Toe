2a2dfe2ebe95ecfdd4af0b5e3a93056ee7108bf7
daceyforward <dacey.loving@gmail.com>
2023-07-30 15:08:55 -0400

    trial commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 /*----- constants -----*/
 
 
+
+
 /*----- state variables -----*/
+
+
 
 
 /*----- cached elements  -----*/
 
 
-/*----- event listeners -----*/
 
 
 /*----- functions -----*/
+
+
+
+
+/*----- event listeners -----*/
+
+
 
